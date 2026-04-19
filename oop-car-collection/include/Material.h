b12aa81d9b2b102347e79f48645b6f56fafd5f25
Казаков Егор Андреевич <egankazakov@edu.hse.ru>
2026-04-20 02:14:25 +0300

    Add OOP car collection project

---
+++ oop-car-collection/include/Material.h
@@ -0,0 +1,13 @@
+#ifndef OOP_1_1_MATERIAL_H
+#define OOP_1_1_MATERIAL_H
+
+
+enum class Material {
+    Wood,
+    Metal,
+    Glass,
+    MDF,
+    Plastic
+};
+
+#endif
---
+++ oop-car-collection/include/ProcessMemory.h
@@ -0,0 +1,7 @@
+#ifndef OOP_1_1_PROCESSMEMORY_H
+#define OOP_1_1_PROCESSMEMORY_H
+
+
+void printProcessMemory();
+
+#endif
